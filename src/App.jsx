c411750cc73fed376aaf6011dfbd1ee0a6059e86
Jonathan <jdvega074@soy.sena.edu.com>
2025-01-11 12:29:25 -0500

    se agrega pagina de nosotros

--- src/App.jsx
+++ src/App.jsx
@@ -175,7 +175,7 @@ function App() {
       <footer className="bg-white rounded-lg shadow m-4 w-4/5 mx-auto">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2024 <a href="https://flowbite.com/" className="hover:underline">Vidrio al Arte S.A.S</a>. TODOS LOS DERECHOS RESERVADOS.
+          © 2024 <a href="https://flowbite.com/" className="hover:underline">Vidrio al Arte SAS. Cl. 71A #75 36, Bogotá</a>. TODOS LOS DERECHOS RESERVADOS.
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
--- src/components/nosotros.jsx
+++ src/components/nosotros.jsx
@@ -1,11 +1,63 @@
 
 import '../css/colosal.css'; // Archivo CSS para estilos
-
+import { GlobeAmericasIcon, EyeIcon } from '@heroicons/react/20/solid'
+import mision from '../img/img_nosotros/mision.png'
+const features = [
+  {
+    name: 'Misión.',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: GlobeAmericasIcon,
+  },
+  {
+    name: 'Visión.',
+    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+    icon: EyeIcon,
+  },
+]
 export function Nosotros() {
 
   return (
     <>
+    <br />
+ <div className="overflow-hidden bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          <div className="lg:pr-8 lg:pt-4">
+            <div className="lg:max-w-lg">
+              <h2 className="text-base/7 font-semibold text-cyan-500">Vidrio al Arte SAS</h2>
+              <p className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-700 sm:text-5xl">
+                Misión y Visión
+              </p>
+              <p className="mt-6 text-lg/8 text-default-400">
+              En Vidrio al Arte SAS, nos guiamos por principios que reflejan nuestro compromiso con la excelencia y la innovación. 
+              Creemos en la importancia de ofrecer productos de alta calidad y en la satisfacción total de nuestros clientes. 
+              Nuestra dedicación y pasión por el arte del vidrio nos impulsan a superar constantemente nuestras propias expectativas.
 
+              </p>
+              <dl className="mt-10 max-w-xl space-y-8 text-base/7 text-default-400 lg:max-w-none">
+                {features.map((feature) => (
+                  <div key={feature.name} className="relative pl-9">
+                    <dt className="inline font-semibold text-gray-700">
+                      <feature.icon aria-hidden="true" className="absolute left-1 top-1 size-5 text-cyan-600" />
+                      {feature.name}
+                    </dt>{' '}
+                    <dd className="inline">{feature.description}</dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
+          <img
+            alt="Product screenshot"
+            src={mision}
+            width={2432}
+            height={1442}
+            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+          />
+        </div>
+      </div>
+    </div>
     </>
   );
 }
