9c77be6a935c333c253fd3afdc6cd08156134605
Jonathan <jdvega074@soy.sena.edu.com>
2025-01-20 14:11:16 -0500

    se agregan espejos organicos

--- src/App.jsx
+++ src/App.jsx
@@ -90,6 +90,7 @@ import { Sensoresc } from './pages/skyleds/sensorec/sensoresc';
 import { Sensorese } from './pages/skyleds/sensorese/sensorese';
 import { Espejos } from './pages/espejos/espejos';
 import { Redondos } from './pages/espejos/redondos/redondos';
+import { Cuadrados } from './pages/espejos/cuadrados/cuadrados';
 
 const cookies = new Cookies();
 
@@ -257,6 +258,7 @@ function App() {
           <Route path="p2/espe1" element={<EspejoRedondo />} />
           <Route path='espejos' element={<Espejos />} />
           <Route path='espejos/espdis1' element={<Redondos />} />
+          <Route path='espejos/espdis2' element={<Cuadrados />} />
           <Route path="p3" element={<TiposAccesorios />} />
           <Route path="p3/ac1" element={<Batientes />} />
           <Route path="p3/ac2" element={<Bisagras />} />
