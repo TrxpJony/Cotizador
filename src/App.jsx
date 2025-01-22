4709c5f8956885dbdf6a45d1fe6e7d46087dbaea
Jonathan <jdvega074@soy.sena.edu.com>
2025-01-22 11:33:45 -0500

    se agregan imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -100,6 +100,7 @@ import { Sandblasting } from './pages/sandblasting/sandblasting';
 import { SandblastingFruteros } from './pages/sandblasting/fruteros/fruteros';
 import { SandblastingFlores } from './pages/sandblasting/flores/flores';
 import { SandblastingAnimales } from './pages/sandblasting/animales/animales';
+import { SandblastingEsquineros } from './pages/sandblasting/esquineros/esquineros';
 
 const cookies = new Cookies();
 
@@ -306,6 +307,7 @@ function App() {
           <Route path="sandblasting/sand1" element= {<SandblastingFruteros />}/>
           <Route path="sandblasting/sand2" element= {<SandblastingFlores/>}/>
           <Route path="sandblasting/sand3" element= {<SandblastingAnimales/>}/>
+          <Route path="sandblasting/sand4" element= {<SandblastingEsquineros/>}/>
         </Routes>
       </div>
       <footer className="bg-white rounded-lg shadow m-4 w-4/5 mx-auto">
