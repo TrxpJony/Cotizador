4e6770af621ee1c49a12f36d9f1a7b6ed78cd54a
Jonathan <jdvega074@soy.sena.edu.com>
2025-03-06 16:51:07 -0500

    se prueba productos

--- src/components/products/ProductsTable.jsx
+++ src/components/products/ProductsTable.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { toast, ToastContainer } from 'react-toastify';
 import Swal from 'sweetalert2'; // Importar SweetAlert2
 
-const baseUrl = 'http://localhost:3002/api/detalleProductos';
+const baseUrl = import.meta.env.VITE_API_URL + '/api/detalleProductos';
 
 const ProductsTable = () => {
 	const [products, setProducts] = useState([]);
