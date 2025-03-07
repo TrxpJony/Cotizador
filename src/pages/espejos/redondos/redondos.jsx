5e1fd905cac2fc80b3da627e3ced564c261a6aca
Jonathan <jdvega074@soy.sena.edu.com>
2025-03-07 10:00:25 -0500

    se prueba ordenar por nombres

--- src/pages/espejos/redondos/redondos.jsx
+++ src/pages/espejos/redondos/redondos.jsx
@@ -20,8 +20,10 @@ export function Redondos() {
             .then((response) => response.json())
             .then((data) => {
                 if (data && Array.isArray(data)) {
-                    // Filtrar los datos para que solo se muestren los de categoria ""
+                    // Filtrar los datos para que solo se muestren los de categoria "redondos"
                     const categoriaData = data.filter(item => item.categoria?.toLowerCase() === 'redondos');
+                    // Ordenar los datos por el nombre
+                    categoriaData.sort((a, b) => a.title.localeCompare(b.title));
                     setList(categoriaData);
                     setFilteredList(categoriaData);
                 } else {
