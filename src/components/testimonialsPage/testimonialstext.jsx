151d77331a9dae37013413d0af5e77cbde31cac3
Jonathan <jdvega074@soy.sena.edu.com>
2025-03-11 09:07:23 -0500

    se cambia ruta en espejos

--- src/components/testimonialsPage/testimonialstext.jsx
+++ src/components/testimonialsPage/testimonialstext.jsx
@@ -1,19 +1,25 @@
 import '../../css/colosal.css'; // Archivo CSS para estilos
+import { motion } from 'framer-motion'; // Importar framer-motion
 
 const TestimonialsText = () => {
     return (
         <>
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <motion.div 
+                className="mx-auto max-w-7xl px-6 lg:px-8"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1 }}
+            >
                 <div className="mx-auto max-w-2xl lg:mx-0">
                     <h2 className="text-base/7 font-semibold text-gray-600 ">Vidrio al Arte SAS</h2>
                     <p className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-700 sm:text-5xl">
                         Proyectos
                     </p>
                     <p className="mt-2 text-lg/8 text-gray-600">
-                        En Vidrio al Arte SAS, nos especializamos en una amplia gama de proyectos que incluyen la instalación de divisiones de baño, espejos personalizados, productos de vidrio, equipos especializados y trabajos en acero. Nuestro equipo de expertos trabaja en estrecha colaboración con los clientes para garantizar que cada proyecto se realice con la más alta calidad y atención al detalle. Ya sea que estés renovando tu hogar o desarrollando un nuevo espacio comercial, estamos aquí para ayudarte a transformar tus ideas en realidad con soluciones innovadoras y acabados profesionales.
+                        En Vidrio al Arte SAS, nos especializamos en proyectos de instalación de divisiones de baño, espejos personalizados, productos de vidrio y trabajos en aluminio. Nuestro equipo trabaja en estrecha colaboración con los clientes para garantizar la más alta calidad y atención al detalle. Ya sea que estés renovando tu hogar o desarrollando un nuevo espacio comercial, estamos aquí para transformar tus ideas en realidad con soluciones innovadoras y acabados profesionales.
                     </p>
                 </div>
-            </div>
+            </motion.div>
         </>
     );
 };
