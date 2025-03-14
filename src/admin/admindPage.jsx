23a6d5b437f00c17ebafaba0df8972200e5e67ff
Jonathan <jdvega074@soy.sena.edu.com>
2025-03-14 16:50:20 -0500

    se prueba arreglar error de sidebar

--- src/admin/admindPage.jsx
+++ src/admin/admindPage.jsx
@@ -5,8 +5,8 @@ const AdmindPage = () => {
     return (
         <>
 		<div className='flex h-screen overflow-hidden'>
-        			<Sidebar className='w-1/4 md:w-1/5 lg:w-1/6' />
-					<div className='flex-grow overflow-auto'>
+        			<Sidebar className='w-1/4 md:w-1/5 lg:w-1/6 h-full' />
+					<div className='flex-grow overflow-auto h-full'>
 						<Info/>
 					</div>
 		</div>
