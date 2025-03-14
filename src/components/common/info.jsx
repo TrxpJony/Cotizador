612673dae1b51777da1a904edcb17e2bad4e4e16
Jonathan <jdvega074@soy.sena.edu.com>
2025-03-14 16:41:15 -0500

    se eliminan br innesesarios

--- src/components/common/info.jsx
+++ src/components/common/info.jsx
@@ -30,8 +30,6 @@ const features = [
 export default function Example() {
   return (
     <>
-      <br />
-      <br />
       <div className="bg-white py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:text-center">
