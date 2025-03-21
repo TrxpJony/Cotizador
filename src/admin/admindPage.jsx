735e81c14d76841abd285dbeffc714044c3faf05
Jonathan <jdvega074@soy.sena.edu.com>
2025-03-21 14:46:21 -0500

    se editan estilos de sidebar y de admin page que para que no quede flotando

--- src/admin/admindPage.jsx
+++ src/admin/admindPage.jsx
@@ -2,15 +2,15 @@ import Sidebar from "../components/common/Sidebar";
 import Info from "../components/common/info";
 const AdmindPage = () => {
 
-	return (
-		<>
-			<div className='flex h-screen overflow-hidden'>
-				<Sidebar className='w-1/4 md:w-1/5 lg:w-1/6 h-full' />
-				<div className='flex-grow overflow-auto h-full'>
-					<Info />
-				</div>
-			</div>
+    return (
+        <>
+		<div className='flex h-full overflow-hidden'>
+        			<Sidebar className='w-1/4 md:w-1/5 lg:w-1/6' />
+					<div className='flex-grow overflow-auto'>
+						<Info/>
+					</div>
+		</div>
 		</>
-	);
+    );
 };
 export default AdmindPage;
