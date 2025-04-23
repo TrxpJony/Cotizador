fa75b60277fcc1a3f59c1f085fd0903eea67871b
Jonathan <jdvega074@soy.sena.edu.com>
2025-04-23 09:37:47 -0500

    se agrega funcionalidad al formulario de contacto

--- src/components/contact/form.jsx
+++ src/components/contact/form.jsx
@@ -2,9 +2,67 @@ import { useState } from 'react'
 import { ChevronDownIcon } from '@heroicons/react/16/solid'
 import { Field, Label, Switch } from '@headlessui/react'
 import { motion } from 'framer-motion';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const baseUrl = import.meta.env.VITE_API_URL + "/api/send-question";
 
 export default function FormContact() {
     const [agreed, setAgreed] = useState(false)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const data = {
+            nombre: formData.get("first-name"),
+            apellido: formData.get("last-name"),
+            email: formData.get("email"),
+            telefono: formData.get("phone-number"),
+            mensaje: formData.get("message"),
+        };
+
+        try {
+            const res = await fetch(baseUrl, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(data),
+            });
+            if (res.ok) {
+                toast.success("Tu mensaje fue enviado con éxito. Pronto te contactaremos.", {
+                    position: "bottom-center",
+                    autoClose: 5000,
+                    hideProgressBar: true,
+                    closeOnClick: false,
+                    pauseOnHover: true,
+                    draggable: true,
+                    valueOftheme: "light"
+                });
+                e.target.reset(); // Limpiar el formulario si fue exitoso
+            } else {
+                toast.error("Hubo un problema al enviar tu mensaje.", {
+                    position: "bottom-center",
+                    autoClose: 5000,
+                    hideProgressBar: true,
+                    closeOnClick: false,
+                    pauseOnHover: true,
+                    draggable: true,
+                    theme: "light",
+                });
+            }
+        } catch (err) {
+            console.error("Error enviando mensaje:", err);
+            toast.error("Error de red al enviar tu mensaje.", {
+                position: "bottom-center",
+                autoClose: 5000,
+                hideProgressBar: true,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                theme: "light",
+            });
+        }
+    };
 
     return (
         <>
@@ -37,7 +95,7 @@ export default function FormContact() {
                         <h2 className="text-balance text-4xl font-semibold tracking-tight text-white sm:text-5xl">¿Como podemos ayudarte?</h2>
                         <p className="mt-2 text-lg/8 text-gray-300">Por favor, completa el formulario y nos pondremos en contacto contigo lo antes posible.</p>
                     </div>
-                    <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20 px-4 sm:px-6">
+                    <form onSubmit={handleSubmit} className="mx-auto mt-16 max-w-xl sm:mt-20 px-4 sm:px-6">
                         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-1 md:grid-cols-2">
                             <div>
                                 <label htmlFor="first-name" className="block text-sm/6 font-semibold text-white">
@@ -96,9 +154,7 @@ export default function FormContact() {
                                                 aria-label="Country"
                                                 className="col-start-1 row-start-1 w-full appearance-none rounded-md py-2 pl-3.5 pr-7 text-base text-gray-500 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-cyan-500 sm:text-sm/6"
                                             >
-                                                <option>US</option>
-                                                <option>CA</option>
-                                                <option>EU</option>
+                                                <option>+57</option>
                                             </select>
                                             <ChevronDownIcon
                                                 aria-hidden="true"
@@ -146,7 +202,7 @@ export default function FormContact() {
                                 <Label className="text-sm/6 text-gray-300">
                                     Al seleccionar esto, aceptas nuestra{' '}
                                     <a href="#" className="font-semibold text-cyan-500">
-                                    política&nbsp;de privacidad
+                                        política&nbsp;de privacidad
                                     </a>
                                     .
                                 </Label>
@@ -161,6 +217,7 @@ export default function FormContact() {
                             </button>
                         </div>
                     </form>
+                    <ToastContainer />
                 </div>
             </motion.div>
         </>
