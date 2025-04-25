5837e7e509c6602f3270c2d73cd769239aa333ed
Jonathan <jdvega074@soy.sena.edu.com>
2025-04-25 09:38:14 -0500

    se agregan links a colaboradores

--- src/components/inicio/colaboracion.jsx
+++ src/components/inicio/colaboracion.jsx
@@ -6,6 +6,7 @@ import logo5 from '../../img/img_Principal/5.png';
 import logo6 from '../../img/img_Principal/6.png'
 import '../../css/colosal.css'; // Archivo CSS para estilos
 
+
 export function Colaboración() {
 
   return (
@@ -16,48 +17,60 @@ export function Colaboración() {
             En colaboración con
           </h2>
           <div className="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-6">
-            <img
-              alt="Espejos SAS"
-              src={logo1}
-              width={158}
-              height={48}
-              className="col-span-2 max-h-40 w-full object-contain lg:col-span-1"
-            />
-            <img
-              alt="Vidrio Andino"
-              src={logo2}
-              width={158}
-              height={48}
-              className="col-span-2 max-h-40 w-full object-contain lg:col-span-1"
-            />
-            <img
-              alt="Sky Leds"
-              src={logo3}
-              width={158}
-              height={48}
-              className="col-span-2 max-h-40 w-full object-contain lg:col-span-1"
-            />
-            <img
-              alt="Vitral"
-              src={logo4}
-              width={158}
-              height={48}
-              className="col-span-2 max-h-40 w-full object-contain sm:col-start-2 lg:col-span-1"
-            />
-            <img
-              alt="Aluminark"
-              src={logo5}
-              width={158}
-              height={48}
-              className="col-span-2 max-h-40 w-full object-contain lg:col-span-1"
-            />
-            <img
-              alt="Vitelsa"
-              src={logo6}
-              width={158}
-              height={48}
-              className="col-span-2 max-h-40 w-full object-contain sm:col-start-2 lg:col-span-1"
-            />
+            <a href="https://espejossas.com/" target="_blank" rel="noopener noreferrer">
+              <img
+                alt="Espejos SAS"
+                src={logo1}
+                width={158}
+                height={48}
+                className="col-span-2 max-h-40 w-full object-contain lg:col-span-1 cursor-pointer"
+              />
+            </a>
+            <a href="https://www.vidrioandino.com" target='_blank' rel='noopener noreferrer'>
+              <img
+                alt="Vidrio Andino"
+                src={logo2}
+                width={158}
+                height={48}
+                className="col-span-2 max-h-40 w-full object-contain lg:col-span-1 cursor-pointer"
+              />
+            </a>
+            <a href='https://linktr.ee/Acervid1' target='_blank' rel='noopener noreferrer'>
+              <img
+                alt="Sky Leds"
+                src={logo3}
+                width={158}
+                height={48}
+                className="col-span-2 max-h-40 w-full object-contain lg:col-span-1 cursor-pointer"
+              />
+            </a>
+            <a href="https://vitral.com.co/" target='_blank' rel='noopener noreferrer'>
+              <img
+                alt="Vitral"
+                src={logo4}
+                width={158}
+                height={48}
+                className="col-span-2 max-h-40 w-full object-contain sm:col-start-2 lg:col-span-1 cursor-pointer"
+              />
+            </a>
+            <a href="https://aluminark.com/" target='_black' rel='noopener noreferrer'>
+              <img
+                alt="Aluminark"
+                src={logo5}
+                width={158}
+                height={48}
+                className="col-span-2 max-h-40 w-full object-contain lg:col-span-1 cursor-pointer"
+              />
+            </a>
+            <a href="https://vitelsa.com.co/" target='_black' rel='noopener noreferrer'>
+              <img
+                alt="Vitelsa"
+                src={logo6}
+                width={158}
+                height={48}
+                className="col-span-2 max-h-40 w-full object-contain sm:col-start-2 lg:col-span-1 cursor-pointer"
+              />
+            </a>
           </div>
         </div>
       </div>
