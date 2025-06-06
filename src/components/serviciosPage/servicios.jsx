1b8ef29c1c19f99c4f6d4dd780ab34a521a4d993
Jonathan <jdvega074@soy.sena.edu.com>
2025-06-06 11:26:26 -0500

    se arreglan los estilos en pagina de servicio para pantallas medianas

--- src/components/serviciosPage/servicios.jsx
+++ src/components/serviciosPage/servicios.jsx
@@ -123,11 +123,11 @@ const Servicios = () => {
                         Nos especializamos en
                     </h2>
                 </div>
-                <div className='mx-auto mb-10 mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-8 lg:mx-0 lg:max-w-none lg:grid-cols-3'>
+                <div className='mx-auto mb-10 mt-10 grid w-full max-w-2xl grid-cols-1 gap-x-8 gap-y-8 lg:mx-0 lg:max-w-none lg:grid-cols-3'>
                     {posts.map((post) => (
                         <motion.article
                             key={post.id}
-                            className='flex max-w-xl flex-col items-start justify-between transform transition-transform hover:scale-105 bg-white shadow-lg rounded-lg overflow-hidden'
+                            className='flex  flex-col items-start justify-between transform transition-transform hover:scale-105 bg-white shadow-lg rounded-lg overflow-hidden'
                             initial={{ opacity: 0, y: 30 }}
                             animate={{ opacity: 1, y: 0 }}
                             transition={{ duration: 0.5, delay: post.id * 0.2 }}
